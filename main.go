70f42bc346852b3a1903e4e22a084a3220cf60c6
CookieNyanCloud <emil8yunusov@gmail.com>
2022-02-02 01:34:16 +0300

    save data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -24,6 +25,11 @@ func main() {
 			fmt.Printf("local env: %v\n", err)
 		}
 	}
+	file, err := os.OpenFile("data.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		fmt.Printf("open file: %v\n", err)
+	}
+	defer file.Close()
 	token := os.Getenv("TG_TOKEN")
 	conf, err := strconv.ParseInt(os.Getenv("TG_CONF"), 10, 64)
 	if err != nil {
@@ -32,6 +38,10 @@ func main() {
 	bot, updates := tg.StartSotaBot(token)
 	last := time.Now().Add(-50 * time.Hour)
 	all := make([]*durafmt.Durafmt, 0)
+	err = readDateFile(all)
+	if err != nil {
+		fmt.Printf("read: %v\n", err)
+	}
 	for update := range updates {
 		if update.Message == nil {
 			continue
@@ -46,6 +56,10 @@ func main() {
 			_, _ = bot.Send(msg)
 			all = append(all, srach)
 			last = time.Now()
+			err := saveDate(file, srach)
+			if err != nil {
+				fmt.Printf("write: %v\n", err)
+			}
 			continue
 		}
 		if !update.Message.IsCommand() {
@@ -59,6 +73,10 @@ func main() {
 			_, _ = bot.Send(msg)
 			all = append(all, srach)
 			last = time.Now()
+			err := saveDate(file, srach)
+			if err != nil {
+				fmt.Printf("write: %v\n", err)
+			}
 		case "last":
 			text := fmt.Sprintf("с последнего срача %v", durafmt.Parse(time.Since(last)))
 			msg := tgbotapi.NewMessage(conf, text)
@@ -73,4 +91,30 @@ func main() {
 			_, _ = bot.Send(msg)
 		}
 	}
+}
+
+func saveDate(file *os.File, date *durafmt.Durafmt) error {
+	_, err := file.WriteString(date.String())
+	return err
+}
+
+func readDateFile(date []*durafmt.Durafmt) error {
+	file, err := os.Open("data.txt")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line, err := durafmt.ParseString(scanner.Text())
+		if err != nil {
+			return err
+		}
+		date = append(date, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	return nil
 }
